bd6613c18beff355f1e888d13ed72de5c9945203
Raman Tank <ramantank04022002@gmail.com>
2024-07-12 16:48:27 +0530

    chore: Remove unnecessary code for project home page

--- apps/studio/pages/project/[ref]/index.tsx
+++ apps/studio/pages/project/[ref]/index.tsx
@@ -30,17 +30,6 @@ const Home: NextPageWithLayout = () => {
 
   return (
     <div>
-      <div className="w-full mx-auto my-16 space-y-16 max-w-7xl">
-        <div className="flex items-center justify-between mx-6 space-x-6">
-          <h1 className="text-3xl">{projectName}</h1>
-          <div className="flex items-center gap-x-3">
-            {/* {IS_PLATFORM && project?.status === PROJECT_STATUS.ACTIVE_HEALTHY && <ServiceStatus />}
-          {IS_PLATFORM && project?.status === PROJECT_STATUS.ACTIVE_HEALTHY && <Connect />} */}
-            <ServiceStatus />
-            <Connect />
-          </div>
-        </div>
-      </div>
       <div className="mx-6">
         <iframe src="http://localhost:3001/status/services" title="Televolution Monitor" style={{ width: '100%', height: '1000px', border: 'none' }}></iframe>
       </div>
