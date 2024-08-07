072e7db6301d924bfd221ba455e66afd61d1ce03
Raman Tank <ramantank04022002@gmail.com>
2024-08-07 19:27:06 +0530

    Removed the supabase files

--- apps/docs/layouts/SiteLayout.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-const SiteLayout = ({ children }: PropsWithChildren<{}>) => {
-  return <main className="grow overflow-hidden">{children}</main>
-}
-
-export default SiteLayout
--- apps/studio/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['eslint-config-supabase/next'],
-  rules: {
-    '@next/next/no-img-element': 'off',
-    'react/no-unescaped-entities': 'off',
-    'react/display-name': 'warn',
-  },
-}
